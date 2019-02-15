066b76312e570b9d3943579bfe7b17a03939f3ef
anieo <ali.maher.1460@gmail.com>
2019-02-16 00:22:30 +0200

    add a file to store the model result in and send it from it

--- app.py
+++ app.py
@@ -7,18 +7,21 @@ from requests_toolbelt import MultipartEncoder
 import os
 app = Flask(__name__, static_url_path='/static')
 api = Api(app)
-
 class Image(Resource):
     def post(self):
         image = request.files.to_dict() #convert multidict to dict
         print(image.keys())
         file_name = image["Input"].filename
+
         if not os.path.isdir("images"):
-                
             os.mkdir("images")
+
         image["Input"].save("images/"+file_name)
-                
-        fields=({'Name': '3',
+        
+        with open('static/result.txt') as text:
+            res = text.readline().split()
+             
+        fields=({'Name': res[0],
                    'Data':'static/homer.jpg'})
         response = app.response_class(
             response=json.dumps(fields),
